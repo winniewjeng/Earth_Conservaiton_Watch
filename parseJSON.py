7aa7fd6c72d9608da9ee8acb1e5f450e3a114851
Winnie Jeng <winniewjeng@gmail.com>
2018-10-20 14:44:58 -0700

    creating dictionary

--- parseJSON.py
+++ parseJSON.py
@@ -1,19 +1,41 @@
-import re, sys, urllib, urllib.request, json, logging, os
+import urllib.request, json
 
+
+# if __name__ == "__main__":
 # api_token = 'api_key=szvbj9zZLao7J4flDav0lgCJoQah0MUFCZEovD58'
 url = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events'
 #
 # with urllib.request.urlopen('http://python.org/').open() as data:
 # with urllib.request.urlopen("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events") as url:
-# http://maps.googleapis.com/maps/api/geocode/json?address=google
-
 #     d = json.loads(data)
 #     print(d)
 
-import urllib.request, json
-with urllib.request.urlopen("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=szvbj9zZLao7J4flDav0lgCJoQah0MUFCZEovD58") as url:
+event_dict = {}
+
+with urllib.request.urlopen(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=szvbj9zZLao7J4flDav0lgCJoQah0MUFCZEovD58") as url:
     data = json.loads(url.read().decode())
-    print(data)
+    # print(data)
+    count = 0
+    for i in data['events']:
+        print(data['events'][count]['title'])
+        print(data['events'][count]['geometries'][0]['coordinates'][0])  # long
+        try:
+            print(data['events'][count]['geometries'][0]['coordinates'][1])  # lat
+        except IndexError:
+            print('No second coordinate')
+
+        # create a tuple as event_dict key that contains my logitude and latitude
+        coord = (data['events'][count]['geometries'][0]['coordinates'][0],
+                 data['events'][count]['geometries'][0]['coordinates'][1])
+
+
+        event_dict = event_dict + {:}
+
+        print()
+        count = count + 1
+
+    print("\nend")
 
 # def response(url):
 #     with urllib.request.urlopen(url) as response:
